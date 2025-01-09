a565fe14e7f9451c9a86f0b19fb340ed275e2201
ymh386 <dbsansgus0602@naver.com>
2025-01-09 09:16:44 +0900

    ..

--- src/com/winter/app/utils/Collections/ListMain2.java
+++ src/com/winter/app/utils/Collections/ListMain2.java
@@ -1,5 +1,19 @@
 package com.winter.app.utils.Collections;
 
+import java.util.ArrayList;
+
 public class ListMain2 {
+	
+	public static void main(String[] args) {
+		//타입이 안전하지 않다
+		//Element 변수
+		//모을려고 하는 타입 선언
+		// <> Generic - 클래스, 메서드
+		ArrayList<Integer> ar = new ArrayList<>();
+		ar.add(3);
+		ar.add(4);
+		
+		int n = ar.get(0);
+	}
 
 }
